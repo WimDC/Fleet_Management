cd7c05d61bf0d25e397d4d9b88287cea1ab6cbcf
WimDC <r0894616@student.thomasmore.be>
2023-03-04 17:17:21 +0100

    first commit

--- src/main/java/be/decuwi/fleetmanagement/controllers/HomeController.java
+++ src/main/java/be/decuwi/fleetmanagement/controllers/HomeController.java
@@ -1,2 +1,21 @@
-package be.decuwi.fleetmanagement.controllers;public class HomeController {
+package be.decuwi.fleetmanagement.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    private final int mySpecialNumber = 729;
+
+    @GetMapping({"/","/home"})
+    public String home(Model model){
+        model.addAttribute("mySpecialNumber",mySpecialNumber);
+        return "home";
+    }
+    @GetMapping("/person")
+    public String person() {
+        return "person";
+    }
 }
